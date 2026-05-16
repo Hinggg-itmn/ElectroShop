21f6ce9ba43910117344a8c911976400cecf5293
Hinggg-itmn <phihunglt2@gmail.com>
2026-05-16 16:59:08 +0700

    Fix: loại bỏ hoàn toàn các file hệ thống và cache ra khỏi repo

--- Controllers/AboutController.cs
+++
@@ -1,9 +0,0 @@
-// Controllers/AboutController.cs
-using Microsoft.AspNetCore.Mvc;
-
-namespace ElectroShop.Controllers;
-
-public class AboutController : Controller
-{
-    public IActionResult Index() => View();
-}
